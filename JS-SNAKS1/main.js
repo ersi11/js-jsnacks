363321eb99e00efe829692134135a7e638dbdf12
ersidimatranxhi <ersidi.matranxhi1@gmail.com>
2022-03-25 16:58:41 +0100

    primo esercizio

---
+++ JS-SNAKS1/main.js
@@ -0,0 +1,12 @@
+let numeroUno = parseInt(prompt("inserisci un numero"));
+let numeroDue = parseInt(prompt("inserisci un numero"));
+
+
+if (numeroUno > numeroDue){
+    alert("il numero maggiore è" + numeroUno);
+} else if (numeroDue>numeroUno){
+    alert("il numero maggiore è" + numeroDue);
+}else {
+    alert("i numeri sono uguali");
+}
+
