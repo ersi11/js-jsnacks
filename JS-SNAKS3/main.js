659c9e192b4094b03079b4cb23f91292e43d28b3
ersidimatranxhi <ersidi.matranxhi1@gmail.com>
2022-03-25 18:01:53 +0100

    esercizio 3

---
+++ JS-SNAKS3/main.js
@@ -0,0 +1,22 @@
+
+
+let numeroDaInserire1 = parseInt(prompt("inserisci un numero"));
+let numeroDaInserire2 = parseInt(prompt("inserisci un numero"));
+let numeroDaInserire3 = parseInt(prompt("inserisci un numero"));
+let numeroDaInserire4 = parseInt(prompt("inserisci un numero"));
+let numeroDaInserire5 = parseInt(prompt("inserisci un numero"));
+let numeroDaInserire6 = parseInt(prompt("inserisci un numero"));
+let numeroDaInserire7 = parseInt(prompt("inserisci un numero"));
+let numeroDaInserire8 = parseInt(prompt("inserisci un numero"));
+let numeroDaInserire9 = parseInt(prompt("inserisci un numero"));
+let numeroDaInserire10 = parseInt(prompt("inserisci un numero"));
+
+let sommaNumeriInseriti = numeroDaInserire1 + numeroDaInserire2 + 
+    numeroDaInserire3 + numeroDaInserire4 + numeroDaInserire5 + 
+    numeroDaInserire6 + numeroDaInserire7 + numeroDaInserire8 + 
+    numeroDaInserire9 + numeroDaInserire10; 
+alert("la somma è" + sommaNumeriInseriti)
+
+    console.log(sommaNumeriInseriti);
+
+
