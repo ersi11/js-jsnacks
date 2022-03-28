b0f810e35e718d8ce130e8e9e642628e16ad5f6d
ersidimatranxhi <ersidi.matranxhi1@gmail.com>
2022-03-28 09:52:33 +0200

    esercizio 8 fine

---
+++ JS-SNAKS8/main.js
@@ -0,0 +1,10 @@
+function sommaNumeri(Arr){
+    let total=0;
+    for (let i = 0; i=0<10; i++){
+        total = total + Arr[i];
+    }
+    console.log(length);
+    return total;
+}
+
+let myArr = [5,7,6,3,4,5,3];
